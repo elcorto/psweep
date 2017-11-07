84861345df35181fe0a957f39b45cbdfa36a5c7e
Steve Schmerler <git@elcorto.com>
2017-11-07 21:25:44 +0100

    INT: bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ print("install_requires: {}".format(install_requires))
 
 setup(
     name='psweep',
-    version='0.1.0',
+    version='0.1.1',
     description='loop like a pro, make parameter studies fun',
     long_description=long_description,
     url='https://github.com/elcorto/psweep',
