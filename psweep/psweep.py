d1c5d1b6f5e389fedb6d5781763332e67f1df646
Steve Schmerler <git@elcorto.com>
2018-07-02 23:23:25 +0200

    API: mv run() -> run_serial(), add multiprocessing run() version
    The new run() version has a somewhat different behavior compared to
    run_serial(). In general, more daily-work boilerplate has moved in,
    apart from the obvious parallel execution.
    
    * `df` as argument is optional. We have a default name and path
      hard-coded (modulo `calc_dir`, see below). This might get more
      flexible later. It loads the old results json database if present and
      appends.
    * It uses df.append(df_row, ignore_index=True) and doesn't
      construct the index in the loop b/c of parallel execution.
    * `tmpsave` is a bool and the name of the temp files are hard-coded.
    * There is `calc_dir`, which is where `tmpsave` files are written to.
    * Instead of _run, we have _run_id (per run() call) and _pset_id (per
      worker() call (each pset)), both are UUIDs. No more numeric numbering.
    * The default is poolsize=1 -> serial execution.

--- psweep/psweep.py
+++ psweep/psweep.py
@@ -1,10 +1,16 @@
 from io import IOBase
 from itertools import product
-import os, copy
+import multiprocessing as mp
+from functools import partial
+import os, copy, uuid
 import pandas as pd
+
+pj = os.path.join
+
 
 dtype_err_msg = ("reading and writing the json with dtype other than object "
                  "will allow Pandas typecasting, which we very much dislike")
+
 
 def df_json_write(df, name, **kwds):
     assert (df.dtypes == object).all(), dtype_err_msg
@@ -66,7 +72,7 @@ def loops2params(loops):
     return [merge_dicts(flatten(entry)) for entry in loops]
 
 
-def run(df, func, params, tmpsave=None, verbose=False):
+def run_serial(df, func, params, tmpsave=None, verbose=False):
     runkey = '_run'
     lastrun = df[runkey].values[-1] if runkey in df.columns else -1
     lastidx = -1 if  len(df.index) == 0 else df.index[-1]
@@ -87,4 +93,65 @@ def run(df, func, params, tmpsave=None, verbose=False):
         if tmpsave:
             _fn = "{tmpsave}.{run}.{idx}".format(tmpsave=tmpsave, run=run, idx=idx)
             df_json_write(df, _fn)
+    return df
+
+# tmpsave: That's cool, but when running in parallel, we loose the ability to
+# store the whole state of the study calculated thus far. For that we would
+# need an extra thread that periodically checks for or -- even better -- gets
+# informed by workers about finished work and collects the so-far written temp
+# results into a global df -- maybe useful for monitoring progress.
+
+def worker_wrapper(pset, worker, tmpsave=False, verbose=False, run_id=None,
+                   calc_dir=None):
+    assert run_id is not None
+    assert calc_dir is not None
+    pset_id = str(uuid.uuid4())
+    # for printing only
+    df_row = pd.DataFrame([pset], dtype=object)
+    if isinstance(verbose, bool) and verbose:
+        print(df_row)
+    elif is_seq(verbose):
+        print(df_row[verbose])
+    _pset = copy.deepcopy(pset)
+    update = {'_run_id': run_id,
+              '_pset_id': pset_id,
+              '_calc_dir': calc_dir,
+              }
+    _pset.update(update)
+    _pset.update(worker(_pset))
+    df_row = pd.DataFrame([_pset], dtype=object)
+    if tmpsave:
+        fn = pj(calc_dir, 'tmpsave', run_id, pset_id + '.json')
+        os.makedirs(os.path.dirname(fn), exist_ok=True)
+        df_json_write(df_row, fn)
+    return df_row
+    
+
+def run(worker, params, df=None, poolsize=1, tmpsave=False, verbose=False,
+        calc_dir='calc'):
+    results_fn = pj(calc_dir, 'results.json')
+    os.makedirs(calc_dir, exist_ok=True)
+    
+    if df is None:
+        if os.path.exists(results_fn):
+            # TODO: backup .. etc, all the usual boilerplate
+            df = df_json_read(results_fn)
+        else:
+            df = pd.DataFrame()
+
+    run_id = str(uuid.uuid4())
+    worker_wrapper_partial = partial(worker_wrapper,
+                                     worker=worker,
+                                     tmpsave=tmpsave,
+                                     verbose=verbose,
+                                     run_id=run_id,
+                                     calc_dir=calc_dir,
+                                     )
+    with mp.Pool(poolsize) as pool:
+        results = pool.map(worker_wrapper_partial, params)
+    
+    for df_row in results:
+        df = df.append(df_row, ignore_index=True)
+
+    df_json_write(df, results_fn)
     return df
