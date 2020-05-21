24db4ddb9b58751ea823e7a9dc10eb293060df8b
Steve Schmerler <git@elcorto.com>
2020-05-21 12:15:59 +0200

    DOC: remove unused line in benchmark_shell_command example

--- examples/benchmark_shell_command.py
+++ examples/benchmark_shell_command.py
@@ -5,13 +5,13 @@ and b.
 
 params:
 
-[{'a': 1, 'const': 'const', 'b': 1.0}, 
- {'a': 1, 'const': 'const', 'b': 2.0}, 
- {'a': 2, 'const': 'const', 'b': 1.0}, 
- {'a': 2, 'const': 'const', 'b': 2.0}, 
- {'a': 3, 'const': 'const', 'b': 1.0}, 
- {'a': 3, 'const': 'const', 'b': 2.0}, 
- {'a': 4, 'const': 'const', 'b': 1.0}, 
+[{'a': 1, 'const': 'const', 'b': 1.0},
+ {'a': 1, 'const': 'const', 'b': 2.0},
+ {'a': 2, 'const': 'const', 'b': 1.0},
+ {'a': 2, 'const': 'const', 'b': 2.0},
+ {'a': 3, 'const': 'const', 'b': 1.0},
+ {'a': 3, 'const': 'const', 'b': 2.0},
+ {'a': 4, 'const': 'const', 'b': 1.0},
  {'a': 4, 'const': 'const', 'b': 2.0}]
 """
 
@@ -19,12 +19,12 @@ import timeit, os
 from itertools import product
 from subprocess import run
 from psweep import psweep as ps
-        
+
 
 def func(pset):
-    
+
     cmd_tmpl = """
-        echo "calling func: pset: a={a} b={b} const={const}"; 
+        echo "calling func: pset: a={a} b={b} const={const}";
         sleep $(echo "0.1 * {a} / {b}" | bc -l)
         """
     cmd = cmd_tmpl.format(**pset)
@@ -32,10 +32,9 @@ def func(pset):
                                repeat=3,
                                number=1))
     return {'timing': timing}
-            
-                
+
+
 if __name__ == '__main__':
-    results = 'results.json'
     params = ps.loops2params(product(
             ps.seq2dicts('a', [1,2,3,4]),
             ps.seq2dicts('b', [1.0,2.0]),
