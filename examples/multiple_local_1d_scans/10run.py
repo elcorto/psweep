379dc9d8d4ee757f4963d11e1fe983697b1130bf
Steve Schmerler <git@elcorto.com>
2021-03-21 14:12:51 +0100

    MNT: move multiple* examples to extra dir

--- examples/multiple_local_1d_scans/10run.py
+++ examples/multiple_local_1d_scans/10run.py
@@ -5,9 +5,6 @@
 from the `const` dict. We use merge_dicts() to combine constant and varied
 values. Note that merge_dicts() operates left-to-right, so const must be
 left, i.e. replace const values which are varied.
-
-run:
-    $ rm -rf calc*; ./10multiple*.py; ./20multiple*.py
 """
 
 import random
@@ -15,30 +12,29 @@ import psweep as ps
 
 
 def func(pset):
-    return {'result': random.random() * pset['a'] * pset['b']}
+    return {"result": random.random() * pset["a"] * pset["b"]}
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     print("first run")
 
-    const = {'a': 11111,
-             'b': 55555}
+    const = {"a": 11111, "b": 55555}
 
     params = []
     disp_cols = []
 
-    values = dict(a=[1,2,3], b=[66,77])
+    values = dict(a=[1, 2, 3], b=[66, 77])
 
-    for study,seq in values.items():
+    for study, seq in values.items():
         # [{'a': 1}, {'a': 2}, {'a': 3}]
         # [{'b': 66}, {'b': 77}]
         params_1d = ps.plist(study, seq)
-        this_params = ps.pgrid(params_1d, [{'study': study}])
+        this_params = ps.pgrid(params_1d, [{"study": study}])
         this_params = [ps.merge_dicts(const, dct) for dct in this_params]
         params += this_params
         disp_cols.append(study)
 
-    disp_cols += ['_run_id']
+    disp_cols += ["_run_id"]
     df = ps.run_local(func, params, verbose=disp_cols)
-    print(df[disp_cols + ['result']])
+    print(df[disp_cols + ["result"]])
---
+++ examples/multiple_local_1d_scans/run_example.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -eu
+
+./clean.sh
+
+./10run.py
+./20run.py
