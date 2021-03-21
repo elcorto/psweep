5b6362d8c424af14f0c1bdb01271b7564bd083c7
Steve Schmerler <git@elcorto.com>
2021-03-21 14:51:08 +0100

    MNT: move package source to src/, adapt setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 bindir = "bin"
-with open(os.path.join(here, "README.rst")) as fd:
+with open(os.path.join(here, "README.md")) as fd:
     long_description = fd.read()
 
 setup(
@@ -14,12 +14,14 @@ setup(
         "run a parameter study/sweep/scan, save a database"
     ),
     long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/elcorto/psweep",
     author="Steve Schmerler",
     author_email="git@elcorto.com",
     license="BSD 3-Clause",
     keywords="parameter study sweep scan database pandas",
-    packages=["psweep"],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     install_requires=open("requirements.txt").read().splitlines(),
     scripts=["{}/{}".format(bindir, script) for script in os.listdir(bindir)],
 )
