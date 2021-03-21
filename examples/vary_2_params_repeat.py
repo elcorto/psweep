d6bac6a81d07406cdcca0b3a31ad90a92de343c8
Steve Schmerler <git@elcorto.com>
2021-03-21 02:32:22 +0100

    DOC: examples/vary_2_params_repeat.py: limit columns

--- examples/vary_2_params_repeat.py
+++ examples/vary_2_params_repeat.py
@@ -5,21 +5,31 @@ import psweep as ps
 
 
 def func(pset):
-    return {'result': random.random() * pset['a'] * pset['b']}
+    return {"result": random.random() * pset["a"] * pset["b"]}
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     params = ps.pgrid(
-        ps.plist('a', [1,2,3,4]),
-        ps.plist('b', [8,9]),
-        )
+        ps.plist("a", [1, 2, 3]),
+        ps.plist("b", [8, 9]),
+    )
     # First run.
     df = ps.run_local(func, params)
 
     # Second run.
     params = ps.pgrid(
-        ps.plist('a', [11,22,33,44]),
-        ps.plist('b', [88,99]),
-        )
+        ps.plist("a", [11, 22, 33]),
+        ps.plist("b", [88, 99]),
+    )
     df = ps.run_local(func, params)
-    print(df)
+    cols = [
+        "_run_id",
+        "_pset_id",
+        "_run_seq",
+        "_pset_seq",
+        "_pset_sha1",
+        "a",
+        "b",
+        "result",
+    ]
+    ps.df_print(df[cols])
