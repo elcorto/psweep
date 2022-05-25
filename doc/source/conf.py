2696e40726975918b88815f3f5737430a403d16d
Steve Schmerler <git@elcorto.com>
2022-05-25 13:34:45 +0200

    REL: release 0.7.0

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2022, Steve Schmerler"
 author = "Steve Schmerler"
 
 # The full version, including alpha/beta/rc tags
-release = "0.6.0"
+release = "0.7.0"
 
 
 # -- General configuration ---------------------------------------------------
