a93fd3f96eb96154508713e0defb9cdeb6926d4f
Steve Schmerler <git@elcorto.com>
2023-07-19 16:06:54 +0200

    REL: bump version 0.9.0 -> 0.10.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(here, "README.md")) as fd:
 
 setup(
     name="psweep",
-    version="0.9.0",
+    version="0.10.0",
     description=(
         "loop like a pro, make parameter studies fun: set up and "
         "run a parameter study/sweep/scan, save a database"
