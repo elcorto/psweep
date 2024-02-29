ddeb1e59660e3dc9377f50fa6f0a2c6f72f9a1b3
Steve Schmerler <git@elcorto.com>
2024-02-29 21:26:22 +0100

    MNT: add __version__

--- src/psweep/__init__.py
+++ src/psweep/__init__.py
@@ -1,1 +1,26 @@
+import importlib.metadata
 from .psweep import *
+
+# https://stackoverflow.com/a/67097076
+#
+# The version will be the same as the one in pyproject.toml
+#
+#   [project]
+#   name = "psweep"
+#   version = "X.Y.Z"
+#
+# We could also do it the other way around and define the version string here
+# and then with setuptools at least
+# (https://packaging.python.org/en/latest/guides/single-sourcing-package-version/#single-sourcing-the-version)
+#
+#   [project]
+#   name = "psweep"
+#   dynamic = ["version"]
+#
+#   [tool.setuptools.dynamic]
+#   version = {attr = "package.__version__"}
+#
+# But we don't, so we are able to switch out the build backend, if the
+# need arises.
+#
+__version__ = importlib.metadata.version(__package__)
