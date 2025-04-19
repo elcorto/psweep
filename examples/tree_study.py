eb784237b32d9b9ae68209f2918a2a4ebed7b1b8
Steve Schmerler <git@elcorto.com>
2025-04-19 22:13:53 +0200

    ENH: add tree study example

---
+++ examples/tree_study.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+"""
+Build a "tree" of psets by doing leave calculations (study 2: vary parameter
+c), based on each pset in study 1 ("nodes", vary a and b). This is useful if
+calculations for psets of study 1 are costly and should not be repeated for
+each study 2 pset with params a,b,c.
+
+We use run(..., save=False) to avoid writing and auto-reading calc/database.pk,
+as we would want in an append-to-database type setting (repeat/extend study).
+Here we have *two* studies which have their own database each, but we run all
+in one directory for simplicity.
+"""
+
+import random
+import psweep as ps
+
+
+def func_1(pset):
+    """First study function. Assumed to be a costly calculation."""
+    return {"result_1_": random.random() * pset["a"] * pset["b"]}
+
+
+def func_2(pset):
+    """Second study function. For each row in df_1, do some calculation using
+    result_1_ and a value of "c" defined in the 2nd study.
+    """
+    return {"result_2_": pset["result_1"] + pset["c"]}
+
+
+if __name__ == "__main__":
+    params = ps.pgrid(
+        ps.plist("a", [1, 2]),
+        ps.plist("b", [8, 9]),
+    )
+    # First run.
+    df_1 = ps.run(func_1, params, save=False)
+    ps.df_print(df_1)
+
+    # Show how to extract the varied params easily from a df. Here of course we
+    # could use "params" directly, but we show how to use params_from_df()
+    # since in a real world setting, calculations won't usually live in one
+    # script, but instead read in df_1 from disk, for instance.
+    params_1 = ps.params_from_df(df_1)
+    assert params_1 == params
+
+    # Second run. Here we show an interesting case of using pgrid(). Usually we
+    # give it plists
+    #
+    #   plist("a", [1, 2]) -> [{'a': 1}, {'a': 2}]
+    #   plist("b", [8, 9]) -> [{'b': 8}, {'b': 9}]
+    #
+    # to loop over. In the plist case, each dict has length 1. But plists are
+    # just lists of dicts and pgrid() doesn't care where they come from and
+    # indeed how *long* the dicts are. We use that to link up params_1 from
+    # study 1 and plist("c", [77, 88]) in study 2. Cool, eh? An an additional
+    # trick, we zip() together params_1 and result_1_, since params_from_df()
+    # extracts only pset-type columns, not prefix and postfix ones.
+    params = ps.pgrid(
+        zip(params_1, ps.plist("result_1", df_1.result_1_.values)),
+        ps.plist("c", [77, 88]),
+    )
+    df_2 = ps.run(func_2, params, save=False)
+
+    assert set(df_2.result_1.values) == set(df_1.result_1_.values)
+    assert (df_2.result_2_ == df_2.result_1 + df_2.c).all()
+
+    ps.df_print(df_2)
