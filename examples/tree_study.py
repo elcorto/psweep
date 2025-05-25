0db346e2b2aa076fe5f5d5e49d7ee3f23664342d
Steve Schmerler <git@elcorto.com>
2025-05-25 11:37:09 +0200

    DOC: extend docs in tree_study example

--- examples/tree_study.py
+++ examples/tree_study.py
@@ -6,6 +6,26 @@ c), based on each pset in study 1 ("nodes", vary a and b). This is useful if
 calculations for psets of study 1 are costly and should not be repeated for
 each study 2 pset with params a,b,c.
 
+We reuse result_1_ from study 1 as param result_1 in study 2.
+
+df_1
+a b  result_1_
+1 8   1.430288
+1 9   3.148494
+2 8  15.298595
+2 9   3.704101
+
+df_2
+a b  c   result_1   result_2_
+1 8 77   1.430288   78.430288
+1 8 88   1.430288   89.430288
+1 9 77   3.148494   80.148494
+1 9 88   3.148494   91.148494
+2 8 77  15.298595   92.298595
+2 8 88  15.298595  103.298595
+2 9 77   3.704101   80.704101
+2 9 88   3.704101   91.704101
+
 We use run(..., save=False) to avoid writing and auto-reading calc/database.pk,
 as we would want in an append-to-database type setting (repeat/extend study).
 Here we have *two* studies which have their own database each, but we run all
@@ -35,6 +55,7 @@ if __name__ == "__main__":
     )
     # First run.
     df_1 = ps.run(func_1, params, save=False)
+    print("df_1")
     ps.df_print(df_1)
 
     # Show how to extract the varied params easily from a df. Here of course we
@@ -53,7 +74,7 @@ if __name__ == "__main__":
     # to loop over. In the plist case, each dict has length 1. But plists are
     # just lists of dicts and pgrid() doesn't care where they come from and
     # indeed how *long* the dicts are. We use that to link up params_1 from
-    # study 1 and plist("c", [77, 88]) in study 2. Cool, eh? An an additional
+    # study 1 and plist("c", [77, 88]) in study 2. Cool, eh? As an additional
     # trick, we zip() together params_1 and result_1_, since params_from_df()
     # extracts only pset-type columns, not prefix and postfix ones.
     params = ps.pgrid(
@@ -65,4 +86,5 @@ if __name__ == "__main__":
     assert set(df_2.result_1.values) == set(df_1.result_1_.values)
     assert (df_2.result_2_ == df_2.result_1 + df_2.c).all()
 
+    print("\ndf_2")
     ps.df_print(df_2)
