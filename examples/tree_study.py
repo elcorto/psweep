ce50b8ec1dfba7a58b26d2e65f8b98dbb31c39dc
Steve Schmerler <git@elcorto.com>
2025-07-22 14:53:04 +0200

    DOC: expand tree_study example doc string

--- examples/tree_study.py
+++ examples/tree_study.py
@@ -1,30 +1,80 @@
 #!/usr/bin/env python3
 
 """
-Build a "tree" of psets by doing leave calculations (study 2: vary parameter
-c), based on each pset in study 1 ("nodes", vary a and b). This is useful if
-calculations for psets of study 1 are costly and should not be repeated for
-each study 2 pset with params a,b,c.
+We want to vary params a,b,c. The normal way to do this is to loop over all
+values of a,b and c using pgrid(), which would give us these psets
 
-We reuse result_1_ from study 1 as param result_1 in study 2.
+├─ a=1 b=8 c=77
+├─ a=1 b=8 c=88
+├─ a=1 b=9 c=77
+├─ a=1 b=9 c=88
+├─ a=2 b=8 c=77
+├─ a=2 b=8 c=88
+├─ a=2 b=9 c=77
+└─ a=2 b=9 c=88
+
+and this result database
+
+df
+a b  c   result_ab_  result_abc_
+1 8 77     1.430288    78.430288
+1 8 88     1.430288    89.430288
+1 9 77     3.148494    80.148494
+1 9 88     3.148494    91.148494
+2 8 77    15.298595    92.298595
+2 8 88    15.298595   103.298595
+2 9 77     3.704101    80.704101
+2 9 88     3.704101    91.704101
+
+But now suppose that to calculate result_abc_, we fist need to perform some
+costly calculation with a and b, producing
+
+    result_ab_ = func_ab(a,b)
+
+We store that and do a final calculation with c to produce
+
+    result_abc_ = func_abc(result_ab_, c)
+
+In the case above we'd repeat func_ab() for each value of c, which is
+suboptimal when func_ab() is slow.
+
+Instead, we can build a "tree" of psets by doing leave calculations (study 2:
+vary parameter c), based on each pset in study 1 ("nodes", vary a and b).
+
+We reuse result_ab_ from study 1 as param result_ab in study 2. Here is the tree:
+
+├─ a=1 b=8
+│  ├─ c=77
+│  └─ c=88
+├─ a=1 b=9
+│  ├─ c=77
+│  └─ c=88
+├─ a=2 b=8
+│  ├─ c=77
+│  └─ c=88
+└─ a=2 b=9
+   ├─ c=77
+   └─ c=88
+
+and here the corresponding databases:
 
 df_1
-a b  result_1_
-1 8   1.430288
-1 9   3.148494
-2 8  15.298595
-2 9   3.704101
+a b  result_ab_
+1 8    1.430288
+1 9    3.148494
+2 8   15.298595
+2 9    3.704101
 
 df_2
-a b  c   result_1   result_2_
-1 8 77   1.430288   78.430288
-1 8 88   1.430288   89.430288
-1 9 77   3.148494   80.148494
-1 9 88   3.148494   91.148494
-2 8 77  15.298595   92.298595
-2 8 88  15.298595  103.298595
-2 9 77   3.704101   80.704101
-2 9 88   3.704101   91.704101
+a b  c   result_ab_   result_abc_
+1 8 77     1.430288     78.430288
+1 8 88     1.430288     89.430288
+1 9 77     3.148494     80.148494
+1 9 88     3.148494     91.148494
+2 8 77    15.298595     92.298595
+2 8 88    15.298595    103.298595
+2 9 77     3.704101     80.704101
+2 9 88     3.704101     91.704101
 
 We use run(..., save=False) to avoid writing and auto-reading calc/database.pk,
 as we would want in an append-to-database type setting (repeat/extend study).
@@ -33,19 +83,21 @@ in one directory for simplicity.
 """
 
 import random
+from pprint import pprint
+
 import psweep as ps
 
 
-def func_1(pset):
+def func_ab(pset):
     """First study function. Assumed to be a costly calculation."""
-    return {"result_1_": random.random() * pset["a"] * pset["b"]}
+    return {"result_ab_": random.random() * pset["a"] * pset["b"]}
 
 
-def func_2(pset):
+def func_abc(pset):
     """Second study function. For each row in df_1, do some calculation using
-    result_1_ and a value of "c" defined in the 2nd study.
+    result_ab_ and a value of "c" defined in the 2nd study.
     """
-    return {"result_2_": pset["result_1"] + pset["c"]}
+    return {"result_abc_": pset["result_ab"] + pset["c"]}
 
 
 if __name__ == "__main__":
@@ -53,9 +105,11 @@ if __name__ == "__main__":
         ps.plist("a", [1, 2]),
         ps.plist("b", [8, 9]),
     )
+    print("study 1 params:")
+    pprint(params)
     # First run.
-    df_1 = ps.run(func_1, params, save=False)
-    print("df_1")
+    df_1 = ps.run(func_ab, params, save=False)
+    print("\ndf_1")
     ps.df_print(df_1)
 
     # Show how to extract the varied params easily from a df. Here of course we
@@ -75,16 +129,18 @@ if __name__ == "__main__":
     # just lists of dicts and pgrid() doesn't care where they come from and
     # indeed how *long* the dicts are. We use that to link up params_1 from
     # study 1 and plist("c", [77, 88]) in study 2. Cool, eh? As an additional
-    # trick, we zip() together params_1 and result_1_, since df_extract_params()
+    # trick, we zip() together params_1 and result_ab_, since df_extract_params()
     # extracts only pset-type columns, not prefix and postfix ones.
     params = ps.pgrid(
-        zip(params_1, ps.plist("result_1", df_1.result_1_.values)),
+        zip(params_1, ps.plist("result_ab", df_1.result_ab_.values)),
         ps.plist("c", [77, 88]),
     )
-    df_2 = ps.run(func_2, params, save=False)
+    print("\nstudy 2 params:")
+    pprint(params)
+    df_2 = ps.run(func_abc, params, save=False)
 
-    assert set(df_2.result_1.values) == set(df_1.result_1_.values)
-    assert (df_2.result_2_ == df_2.result_1 + df_2.c).all()
+    assert set(df_2.result_ab.values) == set(df_1.result_ab_.values)
+    assert (df_2.result_abc_ == df_2.result_ab + df_2.c).all()
 
     print("\ndf_2")
     ps.df_print(df_2)
